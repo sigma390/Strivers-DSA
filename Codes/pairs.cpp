29ce52f2a17dd1778864fabeb73fe99f04cc44b9
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-23 18:18:32 +0530

    Some changes

---
+++ Codes/pairs.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    //pairs
+    pair<int,int>p = {1,2};
+    cout<<p.first<<endl<<p.second<<endl;
+    
+    
+    return 0;
+
+}
